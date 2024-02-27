2a907d483f378e421010fbd651470f4475ad0da2
Lucas <lucas@MacBook-Air-de-Lucas.local>
2024-02-27 17:09:00 -0300

    feat: retirando api não utilizada

--- src/app/planetas/page.tsx
+++ src/app/planetas/page.tsx
@@ -1,23 +1,23 @@
 import { PlanetsProvider } from 'src/Providers/PlanetsContext'
 import { PlanetsContainer } from 'src/container/PlanetsContainer'
 
-async function getData(baseUrl: string) {
-  const res = await fetch(`${baseUrl}/api/dimensions`, {
-    next: { revalidate: 60 },
-  })
+// async function getData(baseUrl: string) {
+//   const res = await fetch(`${baseUrl}/api/dimensions`, {
+//     next: { revalidate: 60 },
+//   })
 
-  if (!res) {
-    throw new Error('Failed to fetch data')
-  }
+//   if (!res) {
+//     throw new Error('Failed to fetch data')
+//   }
 
-  return res.json()
-}
+//   return res.json()
+// }
 
 const Planets = async () => {
-  const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'
-  const data = await getData(baseUrl)
+  // const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL || 'http://localhost:3000'
+  // const data = await getData(baseUrl)
   return (
-    <PlanetsProvider dimensions={data}>
+    <PlanetsProvider>
       <PlanetsContainer />
     </PlanetsProvider>
   )
